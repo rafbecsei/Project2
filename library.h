bbfdd9bf865c3e5a21858879dde9025385dcb3a4
Paulo Hirata <127874722+poulison@users.noreply.github.com>
2023-10-19 14:51:21 +0000

    funcao apagar cliente

--- library.h
+++ library.h
@@ -1,2 +1,18 @@
+typedef struct usuario{
+    char nome[100];
+    long cpf;
+    int tipoconta;
+    float valor;
+    int senha;
+} Usuario;
+
+typedef struct{
+    Usuario u[3];
+    int qtd;
+} ListaDeUsuarios;
+
+
+int novousuario(ListaDeUsuarios *lu);
+int apagarusuario(ListaDeUsuarios *lu);
 
 void printMenu();
